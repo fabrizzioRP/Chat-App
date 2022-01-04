a0893edee9f7d04e6dafec1922d58c7b2469c7ee
Fabrizzio <fabrizziorp7@gmail.com>
2022-01-04 20:22:07 -0300

    Change Port

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -2,9 +2,10 @@ import 'dart:io';
 
 class Environments {
   static String apiUrl = Platform.isAndroid
-      ? 'http://192.168.0.3:3000/api'
-      : 'http://localhost:3000/api';
+      ? 'http://192.168.0.3:16999/api'
+      : 'http://localhost:16999/api';
 
-  static String socketUrl =
-      Platform.isAndroid ? 'http://192.168.0.3:3000' : 'http://localhost:3000';
+  static String socketUrl = Platform.isAndroid
+      ? 'http://192.168.0.3:16999'
+      : 'http://localhost:16999';
 }
